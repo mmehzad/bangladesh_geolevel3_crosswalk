69acb340a382396cdf5559a28877dab5ab3612ef
SM Shihab Siddiqui <smshihab@uoregon.edu>
2025-02-06 16:21:00 -0500

    new work

--- SpatialCleaning/spatial_merge.R
+++ SpatialCleaning/spatial_merge.R
@@ -46,11 +46,19 @@ upazillas01 <- st_transform(upazillas01, crs=st_crs(upazillas91))
 upazillas11 <- st_transform(upazillas11, crs=st_crs(upazillas91))
 
 ## Intermediary datasets
+<<<<<<< HEAD
 
 #factory_01_to_91 <- st_intersection(upazillas91, upazillas01) %>%
 #  mutate(overlap_ratio = drop_units(st_area(geometry))/area91) %>%
 #  rename(admin_name.91 = admin_name, admin_name.01 = admin_name.1) %>%
 #  filter(0.005 < overlap_ratio & overlap_ratio < 0.995)
+=======
+<<<<<<< HEAD
+factory_01_to_91 <- st_intersection(upazillas91, upazillas01) %>%
+  mutate(overlap_ratio = drop_units(st_area(geometry))/area91) %>%
+  rename(admin_name.91 = admin_name, admin_name.01 = admin_name.1) %>%
+  filter(0.005 < overlap_ratio & overlap_ratio < 0.995)
+>>>>>>> 9bdc4c9bf635833b7e3fa139f2296409fabf8465
 
 up_11_to_01_inter <- st_intersection(upazillas01, upazillas11) %>%
   
@@ -89,11 +97,34 @@ map_11_to_01_imper <- up_11_to_01_inter %>%
   # upazillas01$admin_name %>% unique() %>% length() == 490
   # This is because there's no significant (> 0.005) intersection of 2011 upz.
   # with the 2001 upz. called "Karnafuli"
+<<<<<<< HEAD
+=======
+=======
+up_11_to_01_inter <- st_intersection(upazillas01, upazillas11) %>%
+  mutate(inter_area = drop_units(st_area(geometry)), overlap_ratio = 100*inter_area/area11) %>%
+  rename(admin_name.01 = admin_name, admin_name.11= admin_name.1)
+
+
+# sum(up_11_to_01_inter$inter_area)/sum(upazillas01$area01) = 1, so all good
+
+map_11_to_01_per <- up_11_to_01_inter %>%
+  
+  # Errors
+  filter(overlap_ratio >99.99)
+
+map_11_to_01_imper <- up_11_to_01_inter %>%
+  
+  # Errors
+  filter(overlap_ratio >0.5 & overlap_ratio <99.99)
+
+# 543 = dim(map_11_to_01_per)[1] + map_11_to_01_imper$admin_name.11 %>% unique() %>% length(), so all good
+>>>>>>> 9bdc4c9bf635833b7e3fa139f2296409fabf8465
 
 # combining un-contained area to a greater 2001 area
 # example: 2011 alfadanga = 62.72% 2001 alfadanga + 32.28% 2001 boalmari
 #   so, we assign all the rows with admin_name.11 == "alfadanga" to a
 #   greater geometry of 2001 alfadanga and 2001 boalmari
+<<<<<<< HEAD
 
 # lets check if the imperfect match data is in the perfect match or not. Robustness check
 
@@ -125,3 +156,11 @@ new_map %>%
 new_map %>%
   ggplot() + 
   geom_sf(aes(fill = as.factor(new_matched)))
+=======
+greater_region_11_to_01_imper <- left_join(map_11_to_01_imper %>% st_drop_geometry(), upazillas01) %>%
+  st_as_sf() %>%
+  group_by(admin_name.11) %>%
+  mutate(greater_geometry = st_union(geometry), admin_name=NULL) %>%
+  ungroup()
+>>>>>>> c6cce65f5e4815c1d5ea7a1ad1d4fa589766d3d9
+>>>>>>> 9bdc4c9bf635833b7e3fa139f2296409fabf8465
