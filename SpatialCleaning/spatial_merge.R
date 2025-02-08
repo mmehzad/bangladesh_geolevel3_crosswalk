23dbd3fca4f31f99b276784149e4d73e4906314c
Mashfique Mehzad <mehzad3160@gmail.com>
2025-02-08 09:20:50 +0600

    cleaned code and greater area

--- SpatialCleaning/spatial_merge.R
+++ SpatialCleaning/spatial_merge.R
@@ -18,21 +18,21 @@ conflict_prefer("filter", "dplyr")
 conflict_prefer("lag", "dplyr")
 
 ## Preparing the data
-upazillas91 <- st_read("./SpatialCleaning/dataset/geo3_bd1991/geo3_bd1991.shp", quiet=TRUE) %>%
+upazillas91 <- st_read("./dataset/geo3_bd1991/geo3_bd1991.shp", quiet=TRUE) %>%
   st_transform(3106) %>%
   select(ADMIN_NAME, contains("IP"), contains("UP"), PARENT) %>%
   clean_names() %>%
   mutate_if(is.character, tolower) %>%
   mutate(area91 = st_area(geometry) %>% drop_units())
 
-upazillas01 <- st_read("./SpatialCleaning/dataset/geo3_bd2001/geo3_bd2001.shp", quiet=TRUE) %>%
+upazillas01 <- st_read("./dataset/geo3_bd2001/geo3_bd2001.shp", quiet=TRUE) %>%
   st_transform(3106) %>%
   select(ADMIN_NAME, contains("IP"), contains("UP"), PARENT) %>%
   clean_names() %>%
   mutate_if(is.character, tolower) %>%
   mutate(area01 = st_area(geometry) %>% drop_units())
 
-upazillas11 <- st_read("./SpatialCleaning/dataset/geo3_bd2011/geo3_bd2011.shp", quiet=TRUE) %>%
+upazillas11 <- st_read("./dataset/geo3_bd2011/geo3_bd2011.shp", quiet=TRUE) %>%
   st_transform(3106) %>%
   select(ADMIN_NAME, contains("IP"), contains("UP"), PARENT) %>%
   clean_names() %>%
@@ -46,38 +46,22 @@ upazillas01 <- st_transform(upazillas01, crs=st_crs(upazillas91))
 upazillas11 <- st_transform(upazillas11, crs=st_crs(upazillas91))
 
 ## Intermediary datasets
-<<<<<<< HEAD
-
-#factory_01_to_91 <- st_intersection(upazillas91, upazillas01) %>%
-#  mutate(overlap_ratio = drop_units(st_area(geometry))/area91) %>%
-#  rename(admin_name.91 = admin_name, admin_name.01 = admin_name.1) %>%
-#  filter(0.005 < overlap_ratio & overlap_ratio < 0.995)
-=======
-<<<<<<< HEAD
-factory_01_to_91 <- st_intersection(upazillas91, upazillas01) %>%
-  mutate(overlap_ratio = drop_units(st_area(geometry))/area91) %>%
-  rename(admin_name.91 = admin_name, admin_name.01 = admin_name.1) %>%
-  filter(0.005 < overlap_ratio & overlap_ratio < 0.995)
->>>>>>> 9bdc4c9bf635833b7e3fa139f2296409fabf8465
 
 up_11_to_01_inter <- st_intersection(upazillas01, upazillas11) %>%
   
   # Containment of 2011 is better captured when 100% of 2011 Up is 2001. To make discussions easier, transformed ratio into %
   mutate(inter_area = drop_units(st_area(geometry)), overlap_ratio = 100*inter_area/area11) %>%
   rename(admin_name.01 = admin_name, admin_name.11= admin_name.1)
-  # filter(0.005 < overlap_ratio & overlap_ratio < 0.995)
 
-## Chahchu, checks like this helps in quality control
-
-# sum(up_11_to_01_inter$inter_area)/sum(upazillas01$area01) = 1
+## Quality Assurance Test (QAT, sounds a lot like CAT =^. .^=)
+sum(up_11_to_01_inter$inter_area)/sum(upazillas01$area01)  ## = 1, so it's all good
 
 # Obtain perfect match
 map_11_to_01_per <- up_11_to_01_inter %>%
   filter(overlap_ratio >99.99) %>%
   mutate(matched = 1)
 
-## Chahchu, checks like this helps in quality control
-
+## QAT
 map_11_to_01_per %>%
   ggplot() + geom_sf(aes(fill = as.factor(matched)))
 
@@ -87,67 +71,43 @@ map_11_to_01_imper <- up_11_to_01_inter %>%
   # True overlap where 2011 is not contained in a previous 2011
   filter(overlap_ratio >0.5 & overlap_ratio <99.5)
 
-# 543 = dim(map_11_to_01_per)[1] + map_11_to_01_imper$admin_name.11 %>% unique() %>% length(), so all good
-
-# So, there are 15 2011 upazilas where they are not contained in any 2001. Rather, they are took from other Upazilas
-# Next, find a way to unite them. If required create a ficticious geolevel3, call it region and define new regions. 
+## QAT
 
   # Note:
   # map_11_to_01$admin_name.01 %>% unique() %>% length() == 489
   # upazillas01$admin_name %>% unique() %>% length() == 490
   # This is because there's no significant (> 0.005) intersection of 2011 upz.
   # with the 2001 upz. called "Karnafuli"
-<<<<<<< HEAD
-=======
-=======
-up_11_to_01_inter <- st_intersection(upazillas01, upazillas11) %>%
-  mutate(inter_area = drop_units(st_area(geometry)), overlap_ratio = 100*inter_area/area11) %>%
-  rename(admin_name.01 = admin_name, admin_name.11= admin_name.1)
+
+dim(map_11_to_01_per)[1] + map_11_to_01_imper$admin_name.11 %>% unique() %>% length()  ## = 543, so all good
 
 
-# sum(up_11_to_01_inter$inter_area)/sum(upazillas01$area01) = 1, so all good
+## combining un-contained area to a greater 2001 area
+## example: 2011 alfadanga = 62.72% 2001 alfadanga + 32.28% 2001 boalmari
+##   so, we assign all the rows with admin_name.11 == "alfadanga" to a
+##   greater geometry of 2001 alfadanga and 2001 boalmari
 
-map_11_to_01_per <- up_11_to_01_inter %>%
-  
-  # Errors
-  filter(overlap_ratio >99.99)
-
-map_11_to_01_imper <- up_11_to_01_inter %>%
-  
-  # Errors
-  filter(overlap_ratio >0.5 & overlap_ratio <99.99)
-
-# 543 = dim(map_11_to_01_per)[1] + map_11_to_01_imper$admin_name.11 %>% unique() %>% length(), so all good
->>>>>>> 9bdc4c9bf635833b7e3fa139f2296409fabf8465
-
-# combining un-contained area to a greater 2001 area
-# example: 2011 alfadanga = 62.72% 2001 alfadanga + 32.28% 2001 boalmari
-#   so, we assign all the rows with admin_name.11 == "alfadanga" to a
-#   greater geometry of 2001 alfadanga and 2001 boalmari
-<<<<<<< HEAD
-
-# lets check if the imperfect match data is in the perfect match or not. Robustness check
-
+## QAT: lets check if the imperfect match data is in the perfect match or not. Robustness check
 inner_join(map_11_to_01_per %>% st_drop_geometry() %>% select(ipum2001, ipum2011), 
           map_11_to_01_imper %>% st_drop_geometry() %>% select(ipum2001, ipum2011)) %>% dim()
+## Dim is 0, so the code below works!
 
-# Dim is 0, so the code below works!
-
-greater_region_11_to_01_imper <- map_11_to_01_imper %>%
-  
-  group_by(ipum2011) %>% # Do not use name please, use ids..
-  summarise(ipum2001 = paste0(ipum2001, collapse=""),
-            geometry = st_union(geometry), admin_name=NULL) %>%
+# We want to keep 2001's geometry. As we'll need to union them
+greater_region_11_to_01_imper <- left_join(map_11_to_01_imper %>% st_drop_geometry(), upazillas01) %>%
+  st_as_sf() %>%
+  group_by(ipum2011) %>%
+  mutate(greater_geometry = st_union(geometry), admin_name=NULL) %>%
   ungroup() %>%
+  select(ipum2001, ipum2011, geometry) %>%
   mutate(matched = 0, new_matched = 1)
 
-map_11_to_01_per %>%
+map_check <- map_11_to_01_per %>%
   select(ipum2011, ipum2001, matched) %>% mutate(new_matched = 0) %>%
-  rbind(greater_region_11_to_01_imper) -> map_check
+  rbind(greater_region_11_to_01_imper) 
 
-map_check %>%
+new_map <- map_check %>%
   group_by(ipum2001) %>%
-  summarise(geometry = st_union(geometry), matched = mean(matched), new_matched = mean(new_matched)) -> new_map
+  summarise(geometry = st_union(geometry), matched = mean(matched), new_matched = mean(new_matched))
 
 new_map %>%
   ggplot() + 
@@ -156,11 +116,3 @@ new_map %>%
 new_map %>%
   ggplot() + 
   geom_sf(aes(fill = as.factor(new_matched)))
-=======
-greater_region_11_to_01_imper <- left_join(map_11_to_01_imper %>% st_drop_geometry(), upazillas01) %>%
-  st_as_sf() %>%
-  group_by(admin_name.11) %>%
-  mutate(greater_geometry = st_union(geometry), admin_name=NULL) %>%
-  ungroup()
->>>>>>> c6cce65f5e4815c1d5ea7a1ad1d4fa589766d3d9
->>>>>>> 9bdc4c9bf635833b7e3fa139f2296409fabf8465
